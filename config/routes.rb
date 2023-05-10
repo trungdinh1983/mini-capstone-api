60b89a559611f369a043838460ac6f21a7a31e73
YOUR NAMETrung <trungdinh1983@gmail.com>
2023-05-10 14:34:12 -0700

    add one product route segment param

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/all_products" => "products#all_products"
+  get "/one_product/:id" => "products#one_product"
 end
